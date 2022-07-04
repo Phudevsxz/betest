f82784b79e019c8d13be9f3eb4c1d6d2247d3ca2
phunguyen1204 <phu.nguyen@orientsoftware.com>
2022-07-04 16:06:01 +0700

    DOmain

---
+++ Domain/Entities/ProjectMember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class ProjectMember:BaseEntity<Guid>
+    {
+        public Guid UserId { get; set; }
+        public Guid ProjectId { get; set; }
+        public bool IsOwner { get; set; }
+        public User User { get; set; }
+        public Project Project { get; set; }
+
+    }
+}
---
+++ Domain/Entities/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Task:BaseEntity<Guid>
+    {
+        public enum statusTypes
+        {
+            New,
+            Inprogress,
+            Finished
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public statusTypes statusType { get; set; }
+        public Guid ProjectId { get; set; }
+        public Guid AssignedUserId { get; set; }
+        public Project Project { get; set; }
+        public User AssignedUser { get; set; }
+    }
+}
